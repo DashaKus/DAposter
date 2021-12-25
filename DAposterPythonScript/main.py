50a1f9956bf1b32f10864095e407d26cdb4cc053
hiccuoboi <denis.shulimovv@gmail.com>
2021-12-25 11:28:26 +0300

    added other_pictures

--- DAposterPythonScript/main.py
+++ DAposterPythonScript/main.py
@@ -1,5 +1,4 @@
 import random
-
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -26,29 +25,44 @@ def writing_text(image, text, x, y):
     cv2.putText(image, text, (x, y), font, 3, (255, 255, 255), 2, cv2.LINE_AA)
     return image
 
-x, y = 30, 100
 
-file = f.readline(open("parameters/backgroung.txt", "r"))
-col = file[0]
-path = 'background/' + col + '.png'
+x = [50, 400, 50, 400]
+y = [50, 50, 350, 350]
+
+with open('parameters/backgroung.txt') as file:
+    col = file.readlines()
+    path = 'background/' + col[0] + '.png'
+    print(path)
+    background = cv2.imread(path)
+    background = resize(background, WIDTH, HEIGHT)
+    file.close()
+
+with open("parameters/other_pictures.txt") as file:
+    list_other_pictures = file.readlines()
+    str1 = " ".join(list_other_pictures)
+    str1 = str1.split()
+    j = 0
+    for i in str1:
+        num = random.randint(1, 20)
+        num = str(num)
+        path = 'other_pictures/' + i + num + '.png'
+        print(path)
+        picture = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        picture = resize(picture, 50, 100)
+        final_image = overlay(background, picture, x[j], y[j])
+        cv2.imwrite("final_image/merged_transparent.png", final_image)
+        j += 1
+    file.close()
+
+num = random.randint(1, 12)
+num = str(num)
+path = 'central_pictures/Mario (' + num + ').png'
 print(path)
-background = cv2.imread(path)
-background = resize(background, WIDTH, HEIGHT)
-
-file = open("parameters/other_pictures", "r")
-print(file)
-for i in range(len(file)):
-    num = random.randint(1, 20)
-    path = 'other_pictures/' + num + '.png'
-    picture = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    picture = resize(picture, 150, 100)
-    final_image = overlay(background, picture, x, y)
-    cv2.imwrite("final_image/merged_transparent.png", final_image)
-
-picture = cv2.imread('central_picture/dog.png', cv2.IMREAD_UNCHANGED)
+picture = cv2.imread(path, cv2.IMREAD_UNCHANGED)
 picture = resize(picture, 250, 200)
 
-final_image = overlay(background, picture, 30, 100)
+
+final_image = overlay(final_image, picture, 200, 150)
 cv2.imwrite("final_image/merged_transparent.png", final_image)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -56,7 +70,3 @@ final_image = writing_text(final_image, 'DAposter', 10, 100)
 cv2.imwrite("final_image/merged_transparent.png", final_image)
 cv2.imshow("Image", final_image)
 cv2.waitKey(0)
-#plt.imshow(final_image)
-#plt.show()
-
-
